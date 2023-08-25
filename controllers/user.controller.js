61d8d47f445f01b2d19c94c7154ea412a5bd1800
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-08-25 10:33:26 +0500

    added User Model

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,12 @@
-
-
 exports.handleSignUp = async (req, res) => {
-  res.send("OK");
+  const { displayName, email, password, confirmPassword } = req.body;
+  if (!email || password)
+    return res
+      .status(400)
+      .json({ message: "Email and password are required!" });
+
+  try {
+  } catch (error) {}
 };
 
 exports.handleSignIn = async (req, res) => {
---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  displayName: String,
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  image: String,
+  roles: {
+    User: {
+      type: Number,
+      default: 2001,
+    },
+    Admin: Number,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
